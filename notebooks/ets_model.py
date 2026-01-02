8a496ea55e2872d4ff61cc69882c566a0d651185
HARSHEE04 <harshetasharma5@gmail.com>
2026-01-02 15:44:56 -0500

    complted ETS Forecast

--- notebooks/ets_model.py
+++ notebooks/ets_model.py
@@ -24,3 +24,49 @@ ets_fit = ets_model.fit()
 #forecast
 ets_forecast= ets_fit.forecast(steps=horizon)
 test['ETS_Forecast'] = ets_forecast.values #put forecasted values on test df under ETS_Forecast for evaluation later on 
+
+
+#perform full forecast on entire data
+y_full= df["GIC Rates"]
+
+ETS_full_model =ETSModel(y_full, error='add', seasonal_periods=12)
+ETS_full_fit = ETS_full_model.fit()
+
+future_steps = 12
+ETS_future_forecast = ETS_full_fit.forecast(steps=future_steps) 
+
+
+#plot the forecast
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x= train.index,
+    y= train['GIC Rates'],
+    mode='lines',
+    name='Train Data'
+))
+
+fig.add_trace(go.Scatter(
+    x= test.index,
+    y= test['GIC Rates'],
+    mode='lines',
+    name='Test Data',
+    line = dict(color= 'black')
+))
+
+fig.add_trace(go.Scatter(x=test.index,
+                        y = test['ETS_Forecast'],
+                        mode='lines',
+                        name='ETS Forecast',
+                        line=dict(dash="dash", color="blue")
+)
+)
+
+fig.update_layout(
+    title ="ETS Model On GIC Rates Searches Using 12 Month Horizon",
+    xaxis_title="Year",
+    yaxis_title="GIC Rates Search Index"
+)
+
+fig.write_image(BASE_DIR / 'figures' / 'etsForecastFig.png')
